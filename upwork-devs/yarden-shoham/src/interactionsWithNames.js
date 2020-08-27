2ad36ffb6b404a8f640b2b30d81f9d93ddf06182
YardenShoham <hrsi88@gmail.com>
2020-08-27 15:14:56 +0300

    Add pricing count test

--- upwork-devs/yarden-shoham/src/interactionsWithNames.js
+++ upwork-devs/yarden-shoham/src/interactionsWithNames.js
@@ -1,8 +1,13 @@
 const PdfTest = require("./pdfTest");
+const PricingCountTest = require("./pricingCountTest");
 
 module.exports = [
   {
     name: "Check PDF is downloadable and correct",
     interaction: PdfTest,
+  },
+  {
+    name: "Check there are at least 8 prices listed",
+    interaction: PricingCountTest,
   },
 ];
