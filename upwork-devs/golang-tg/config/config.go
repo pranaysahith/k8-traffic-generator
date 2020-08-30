ee76fa250eb72e0fe6c07648d6ee46a745e507b9
silvana yousry <silvanayousry28@yahoo.com>
2020-08-30 18:09:08 +0200

    initial commit

---
+++ upwork-devs/golang-tg/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+func Config() time.Duration {
+	if os.Getenv("ENVIROMENT") == "DEV" {
+		viper.SetConfigName("config")
+		viper.SetConfigType("toml")
+		viper.AddConfigPath("github.com/trafficgen")
+		viper.ReadInConfig()
+	} else {
+		viper.AutomaticEnv()
+	}
+	timeout := viper.GetDuration("app_timeout")
+	return timeout
+}
