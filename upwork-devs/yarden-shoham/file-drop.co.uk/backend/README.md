14ef40ae12eee957b5bae3234c01563e3620fe59
YardenShoham <hrsi88@gmail.com>
2020-09-04 00:05:44 +0300

    Add kubernetes scheduling

--- upwork-devs/yarden-shoham/file-drop.co.uk/backend/README.md
+++ upwork-devs/yarden-shoham/file-drop.co.uk/backend/README.md
@@ -1,3 +1,5 @@
 # File Drop Backend
 
 This container hosts a web server at port 5000. It accepts files on `/backend/files` and serves them on `/backend/static`. It saves the static files on `/usr/src/app/backend/static` in the container.
+
+It schedules jobs that interact with File Drop using the API token from the environment variable `API_TOKEN`.
--- upwork-devs/yarden-shoham/file-drop.co.uk/backend/backend/__main__.py
+++ upwork-devs/yarden-shoham/file-drop.co.uk/backend/backend/__main__.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from kubernetes import client, config
 import os
+import uuid
 
 
 class Scheduler:
@@ -7,18 +9,66 @@ class Scheduler:
     def schedule(files):
         for file in files:
             Scheduler.__save_file(file)
+            Scheduler.__schedule_job_in_kubernetes(file)
 
     @staticmethod
     def __save_file(file):
         file.save(f"/usr/src/app/backend/static/{file.filename}")
 
+    @staticmethod
+    def __schedule_job_in_kubernetes(file):
+        job_name = f"file-drop-{uuid.uuid1()}"
+
+        processor_container = client.V1Container(
+            name="processor",
+            image="yardenshoham/file-drop-processor",
+            env=[client.V1EnvVar(
+                name="API_TOKEN", value=os.getenv("API_TOKEN"))],
+            volume_mounts=[client.V1VolumeMount(
+                name="data", mount_path="/input")])
+
+        downloader_container = client.V1Container(
+            name="downloader",
+            image="yardenshoham/file-drop-downloader",
+            env=[client.V1EnvVar(
+                name="TARGET", value=f"http://file-drop-traffic-generator-backend:5000/backend/static/{file.filename}")],
+            volume_mounts=[client.V1VolumeMount(
+                name="data", mount_path="/output")])
+
+        pod_spec = client.V1PodSpec(
+            restart_policy="OnFailure",
+            containers=[processor_container],
+            init_containers=[downloader_container],
+            volumes=[client.V1Volume(name="data", empty_dir=client.V1EmptyDirVolumeSource())])
+
+        # Create and configure a spec section
+        template = client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(name=job_name),
+            spec=pod_spec)
+
+        # Create the specification of the job
+        spec = client.V1JobSpec(
+            template=template)
+
+        # Instantiate the job object
+        job = client.V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=client.V1ObjectMeta(name=job_name),
+            spec=spec)
+
+        client.BatchV1Api().create_namespaced_job(
+            body=job,
+            namespace="default")
+        print(job_name)
+
 
 app = Flask(import_name=__name__, static_url_path="/backend/static")
 
 
-@app.route('/backend/health')
+@app.route("/backend/health")
 def health():
-    return ''
+    return ""
 
 
 @app.route("/backend/files", methods=["POST"])
@@ -29,4 +79,5 @@ def upload():
 
 
 if __name__ == '__main__':
+    config.load_incluster_config()
     app.run(debug=True, host='0.0.0.0')
