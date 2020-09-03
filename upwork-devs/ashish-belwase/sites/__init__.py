b87b8a6cb6a5942ebbb8842080a7ee5f724142f8
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2020-09-03 13:42:11 +0545

    allow generic multiple sites, common site loader, remove hard coded site mapping

---
+++ upwork-devs/ashish-belwase/sites/__init__.py
@@ -0,0 +1,3 @@
+from .base import BaseSite
+from .glasswall import Glasswallsolutions
+from .glasswall_engineering import Engineeringglasswallsolutions
--- upwork-devs/ashish-belwase/traffic_generator.py
+++ upwork-devs/ashish-belwase/traffic_generator.py
@@ -1,29 +1,25 @@
 import logging
 from pyppeteer import launch
-from sites.base import BaseSite
-from sites.glasswall import Glasswallsolutions
+from sites import *
 from helper import Helper
 
 log = logging.getLogger("GW:traffic_g")
 
 
 class TrafficGenerator:
-
-    Allowed_Sites = {"glasswallsolutions.com": Glasswallsolutions}
-
     @staticmethod
     def get_site(url):
+        site = BaseSite
         domain = Helper.get_domain_from_url(url)
-        site = TrafficGenerator.Allowed_Sites.get(domain, None)
+        try:
+            site = globals()[domain.title()]()
+        except:
+            pass
         return site
 
     @staticmethod
     async def run(url, action):
         site = TrafficGenerator.get_site(url)
-        if not site:
-            log.info("requested url is not a registered site")
-            return
-
         if action:
             if action not in site.Allowed_Methods:
                 log.info(f"Invalid action `{action}` on `{site.__name__}`")
@@ -33,7 +29,7 @@ class TrafficGenerator:
 
         log.info(f"starting traffic on : {url} , action : {action}")
 
-        browser = await launch(args=['--no-sandbox'])
+        browser = await launch(args=["--no-sandbox"])
         page = await browser.newPage()
         m = getattr(site, action)
         await m(page, url)
