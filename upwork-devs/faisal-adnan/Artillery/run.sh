461f92d9e50ceb93db8b9b7c0ebaa27385a531b6
pranaysahith <panaysahith7@gmail.com>
2020-09-04 13:21:32 +0530

    update syntax

--- upwork-devs/faisal-adnan/Artillery/run.sh
+++ upwork-devs/faisal-adnan/Artillery/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 CONFIG_FILE=$1
 NUMBER_OF_JOBS=$2
 
@@ -12,10 +12,12 @@ kubectl delete --ignore-not-found secret artilleryconf
 kubectl create secret generic artilleryconf --from-file=artillery-conf.yml
 
 START=1
-for (( c=$START; c<=$NUMBER_OF_JOBS; c++ ))
+c=$START
+while [ $c -le $NUMBER_OF_JOBS ]
 do
     echo "Submitting job $c"
     cat artillery-job-tmpl.yaml | sed "s/\$NO/$c/" > ./jobs/job-$c.yaml
     kubectl create -f ./jobs/job-$c.yaml
     rm ./jobs/job-$c.yaml
+    c=$(( c + 1 ))
 done
