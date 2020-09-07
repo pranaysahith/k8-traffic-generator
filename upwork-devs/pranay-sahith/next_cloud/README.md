0bf04e97c719441c6b08138faa95d10f719831e4
pranaysahith <panaysahith7@gmail.com>
2020-09-07 23:22:39 +0530

    update readme

--- upwork-devs/pranay-sahith/next_cloud/README.md
+++ upwork-devs/pranay-sahith/next_cloud/README.md
@@ -9,7 +9,7 @@ helm upgrade --install nextcloud stable/nextcloud \
   --values nextcloud.values.yml
 ```
 
-## Generate traffic using docker-compose
+## Generate traffic using docker-compose. Next cloud is exposed on port 9090
 
 ```
 docker-compose up --build
@@ -21,3 +21,24 @@ docker-compose up --build
 kubectl apply -f traffic_generator.yaml
 ```
 
+### The pod sends metrics to prometheus and logs to ELK [TBD]
+
+
+```graphviz
+digraph {
+    
+    rankdir=LR
+    
+    n1 [ shape=box, label="shell script"]
+    n2 [ shape=box, label="TG pods 1-N"]
+    n3 [ shape=box, label="Next Cloud"]
+    n4 [ shape=box, label="Prometheus"]
+    n5 [ shape=box, label="ELK"]
+    
+    n1 -> n2
+    n2 -> n3 [label="download/upload files"]
+    n2 -> n4 [label="send metrics"]
+    n2 -> n5 [label="send logs"]
+    
+}
+```
--- upwork-devs/pranay-sahith/next_cloud/traffic_generator.py
+++ upwork-devs/pranay-sahith/next_cloud/traffic_generator.py
@@ -4,25 +4,25 @@ from datetime import datetime
 import os
 import asyncio
 from pyppeteer import launch
-from urllib.parse import quote
 import time
 
-loop = asyncio.get_event_loop()
+# loop = asyncio.get_event_loop()
 log = logging.getLogger("GW:traffic_g")
 
 
 class TrafficGenerator:
 
-    def __init__(self, url, ocp, username, password):
+    def __init__(self, url, ocp, username, password, headless):
         super().__init__()
         self.username = username
         self.password = password
         self.url = url
         self.ocp = ocp
+        self.headless = headless
 
     async def launch_browser(self):
         browser_path = os.getenv("EXECUTABLE_PATH", None)
-        browser = await launch(headless=True, executablePath=browser_path, 
+        browser = await launch(headless=self.headless, executablePath=browser_path, 
                               args=[
             '--no-sandbox',
             '--disable-dev-shm-usage'])
@@ -35,7 +35,7 @@ class TrafficGenerator:
 
     async def login(self):
         # login
-        log.info("login to nextlcloud")
+        log.info("login to nextcloud")
         await self.page.type("input", self.username)
         await self.page.keyboard.press("Tab")
         await self.page.type("input", self.password)
@@ -47,23 +47,25 @@ class TrafficGenerator:
         has_dir = True if len(remote_file_path.split('/')) > 2 else False
         if has_dir:
             dir_name = remote_file_path.split("/")[1]
-            print(f"traversing into dir: {dir_name}")
+            log.info(f"traversing into dir: {dir_name}")
             dir_elements = await self.page.xpath(f"//td//span[@class='innernametext' and text()='{dir_name}']")
             await dir_elements[0].click()
+            log.info(f"traversing into dir: {dir_name}")
             await self.download_file("/" + "/".join(remote_file_path.split("/")[2:]))
         else:
             time.sleep(2)
             file_name = remote_file_path.split("/")[-1]
-            file_element = await self.page.xpath(f"//a[@class='name' and contains(@href,'{file_name}')]/span/a[@class='action action-menu permanent']")
+            name, extension = file_name.split(".")
+            log.info(f"downloading file: {file_name}")
+            file_element = await self.page.xpath(f"//a[span/span/text()='{name}']/span/a[@class='action action-menu permanent']")
             await file_element[0].click()
-            print(f"downloading file: {file_name}")
             download_elements = await self.page.xpath("//li[@class=' action-download-container']/a[@class='menuitem action action-download permanent' and @data-action='Download']")
             await download_elements[0].click()
 
     async def create_dir(self, new_dir_name):
         new_dir_name = new_dir_name.strip("/")
         time.sleep(2)
-        print(f"creating dir: {new_dir_name}")
+        log.info(f"creating dir: {new_dir_name}")
         await self.page.click("span.icon.icon-add")
         await self.page.click("a.menuitem")
         elements = await self.page.xpath("//input[@value='New folder']")
@@ -74,39 +76,40 @@ class TrafficGenerator:
 
     async def open_dir(self, dir_name):
         # open dir
-        print(f"opening dir: {dir_name}")
+        log.info(f"opening dir: {dir_name}")
         dir_elements = await self.page.xpath(f"//td//span[@class='innernametext' and text()='{dir_name}']")
         await dir_elements[0].click()
 
 
     async def upload_file(self, local_file_name):
         # upload
-        print(f"uploading file: {local_file_name}")
+        log.info(f"uploading file: {local_file_name}")
         upload_element = await self.page.xpath("//input[@type='file']")
         await upload_element[0].uploadFile(local_file_name)
 
 
     async def run(self):
         log.info(f"starting traffic on : {self.url}")
-        files = ["/Photos/Coast.jpg", "/Nextcloud intro.mp4", "/Documents/About.txt"]
+        files = ["/Photos/Frog.jpg", "/Nextcloud intro.mp4", "/Documents/Readme.md"]
         await self.launch_browser()
         await self.go_home()
         await self.login()
         time.sleep(5)
-        # for f in files:
-        #     await self.download_file(quote(f))
-        #     # await self.go_home()
-        #     print(f"goto {self.url}")
-        #     await self.page.setCacheEnabled(False)
-        #     response = await self.page.goto(self.url, waitUntil="networkidle0")
-        #     if response is None:
-        #         await self.page.waitForResponse(self.url)
+        for f in files:
+            await self.download_file(f)
+            # await self.go_home()
+            time.sleep(2)
+            log.info(f"goto {self.url}")
+            await self.page.setCacheEnabled(False)
+            response = await self.page.goto(self.url, waitUntil="networkidle0")
+            if response is None:
+                await self.page.waitForResponse(self.url)
             # await self.page.waitForNavigation()
         user = "user_a"
         remote_dir = "/target_dir" + "_" + user
-        # await self.go_home()
-        # await self.create_dir(remote_dir)
-        
+        await self.go_home()
+        await self.create_dir(remote_dir)
+        time.sleep(3)
         upload_files = [f.split("/")[-1] for f in files]
         for dir_name in remote_dir.split("/")[1:]:
             await self.open_dir(dir_name)
