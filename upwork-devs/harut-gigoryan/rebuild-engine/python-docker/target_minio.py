16a68c8061e541928f992bf5fe8a21c5069bfecc
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2020-09-12 14:28:20 +0400

    POD filebeat sends logs to elasticsearch

--- upwork-devs/harut-gigoryan/rebuild-engine/python-docker/target_minio.py
+++ upwork-devs/harut-gigoryan/rebuild-engine/python-docker/target_minio.py
@@ -19,14 +19,14 @@ from minio.error import ResponseError
 class ElkJsonFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
         super(ElkJsonFormatter, self).add_fields(log_record, record, message_dict)
-        log_record['@timestamp'] = datetime.now().isoformat()
+#        log_record['@timestamp'] = datetime.now().isoformat()
         log_record['level'] = record.levelname
         log_record['logger'] = record.name
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(uuid.uuid1().urn)
 
-logging.info('Application running!')
+#logging.info('Application running!')
 
 file_path = '/files/'
 
