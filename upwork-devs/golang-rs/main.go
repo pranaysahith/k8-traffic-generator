6962c2390b92bb2ea1b1ede902934377bde14621
reem-masoud <65132189+reem-masoud@users.noreply.github.com>
2020-09-14 17:38:44 +0200

    Update main.go

--- upwork-devs/golang-rs/main.go
+++ upwork-devs/golang-rs/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/tg/generateurl"
 	"github.com/tg/log"
-	"github.com/tg/upload"
 )
 
 func main() {
-	go func() {
-		log.Filelog("urltxt.txt")
-		generateurl.Thefile("urltxt.txt")
-	}()
-	// Upload route
-	http.HandleFunc("/upload", upload.UploadHandler)
 
-	//Listen on port 8080
-	http.ListenAndServe(":8080", nil)
+	log.Filelog("urltxt.txt")
+
+	generateurl.Thefile("urltxt.txt", "open")
+	generateurl.Thefile("urltxt.txt", "download")
+	generateurl.Thefile("urltxt.txt", "upload")
+
 }
