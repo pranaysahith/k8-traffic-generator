acc6a2ac48e3f89bc3b7b4a1e80066410368f602
YardenShoham <hrsi88@gmail.com>
2020-09-17 10:26:06 +0300

    Support unicode

--- upwork-devs/yarden-shoham/local-rebuild-argo/lister/lister/__main__.py
+++ upwork-devs/yarden-shoham/local-rebuild-argo/lister/lister/__main__.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
     objects = s3client.list_objects(
         bucket_name=bucket_name, recursive=True)
 
-    json.dump([object.object_name for object in objects], sys.stdout)
+    json.dump([object.object_name for object in objects],
+              sys.stdout, ensure_ascii=False)
